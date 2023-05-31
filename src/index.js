089ee85ce7da10e84142eb84ab4b1de6aba0cc11
nikitsch <nikitsch5@gmail.com>
2023-05-31 12:59:16 +0300

    feat: redux library

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import { createStore } from './createStore'
+import { legacy_createStore as createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { asyncIncrement, decrement, increment } from './redux/actions'
 import { rootReducer } from './redux/rootReducer'
 import './styles.css'
 
@@ -8,18 +10,18 @@ const subBtn = document.getElementById("sub")
 const asyncBtn = document.getElementById("async")
 const themeBtn = document.getElementById("theme")
 
-const store = createStore(rootReducer, 0)
+const store = createStore(rootReducer, 0, applyMiddleware(thunk))
 
 addBtn.addEventListener("click", () => {
-  store.dispatch({ type: "INCREMENT" })
+  store.dispatch(increment())
 })
 
 subBtn.addEventListener("click", () => {
-  store.dispatch({ type: "DECREMENT" })
+  store.dispatch(decrement())
 })
 
 asyncBtn.addEventListener("click", () => {
-
+  store.dispatch(asyncIncrement())
 })
 
 themeBtn.addEventListener("click", () => {
