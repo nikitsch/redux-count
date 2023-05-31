ead693e2d0511517a3ffef9cefab792443137b05
nikitsch <nikitsch5@gmail.com>
2023-05-31 15:50:20 +0300

    feat: redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { legacy_createStore as createStore, applyMiddleware } from 'redux'
+import { legacy_createStore as createStore, applyMiddleware, compose  } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import { asyncIncrement, changeTheme, decrement, increment } from './redux/actions'
@@ -23,7 +23,10 @@ const themeBtn = document.getElementById("theme")
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk, logger)
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 )
 
 addBtn.addEventListener("click", () => {
